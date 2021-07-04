73cbec35ac2240c49846902f36e457a21f548585
Bozhidarm <bozhidary@gmail.com>
2021-07-04 19:44:11 +0100

    added api call to save usesrs

--- src/sections/InputComponent.js
+++ src/sections/InputComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import $ from "jquery";
 import { data } from "../assets/Text.js";
 
 
@@ -7,6 +7,21 @@ export const InputComponent = ({ modal, setModal, value, setValue }) => {
   function SubmitMail(e) {
     e.preventDefault();
     setModal(!modal);
+
+    var call = $.ajax({
+      url: "https://starathletepromo.azurewebsites.net/api/Service.svc/saveform",
+      method: "POST",
+      headers: { "Accept": "application/json; odata=verbose", "Content-Type": "application/json" },
+      async: false,
+      crossDomain: true,
+      data: '{ "user_email" : "' + value + '"}',
+      dataType: 'json',
+      success: (response) => {
+        console.log("Response: " + JSON.stringify(response));
+      },
+    }).responseJSON;
+    //console.log("Current form data: " + JSON.stringify(call));
+
   }
   return (
     <form className="input" onSubmit={SubmitMail}>
